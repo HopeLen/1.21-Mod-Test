76c066bc58f36e609374d30d56419d9890a1dcaf
hope <roni.trololo2004@gmail.com>
2025-06-14 21:04:11 +0300

    Added bithmuth and raw bithmuth items. Finished episode 2 of the neoforge tutorial playlist

--- src/main/java/net/hopelen/tutorialmod/TutorialMod.java
+++ src/main/java/net/hopelen/tutorialmod/TutorialMod.java
@@ -1,5 +1,8 @@
 package net.hopelen.tutorialmod;
 
+import net.hopelen.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -31,10 +34,11 @@ public class TutorialMod
         modEventBus.addListener(this::commonSetup);
 
 
-        // Register ourselves for server and other game events we are interested in.
+        // Register ourselves for server and other g ame events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -49,9 +53,11 @@ public class TutorialMod
     }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey()== CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
